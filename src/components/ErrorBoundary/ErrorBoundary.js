925d8042f1dfd0e0450b7753d9ee20afd0f613bd
Vitaliy Derda <derdavitaliy@gmail.com>
2021-05-17 19:39:01 +0300

    feat: added error_boundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,22 @@
+//Core
+import { Component } from 'react';
+
+class ErrorBoundary extends Component {
+	state = {
+		hasError: false,
+	};
+
+	componentDidCatch(error, info) {
+		this.setState({ hasError: true });
+	}
+
+	render() {
+		if (this.state.hasError) {
+			return <h1>Something went wrong, please try again later :(</h1>;
+		}
+
+		return this.props.children;
+	}
+}
+
+export default ErrorBoundary;
